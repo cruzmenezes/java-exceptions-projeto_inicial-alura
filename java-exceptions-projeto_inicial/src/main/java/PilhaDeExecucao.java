a621c7eabb6ec392743d4a8dfa641594d8848124
cruzmenezes <cruz.menezes@outlook.com>
2025-09-23 18:43:57 -0300

    NullPointerException

--- java-exceptions-projeto_inicial/src/main/java/PilhaDeExecucao.java
+++ java-exceptions-projeto_inicial/src/main/java/PilhaDeExecucao.java
@@ -10,8 +10,15 @@ public class PilhaDeExecucao {
     public static void metodo2() {
         System.out.println("[Inicio] - metodo2");
 
-        Usuario usuario = new Usuario("Vinicius");
-        System.out.println(usuario.nome);
+
+        Usuario usuario = null;
+        try {
+            System.out.println(usuario.nome);
+
+        }catch (NullPointerException ex){
+            System.out.println("usuario não encontrado, tente outro usuario");
+        }
+
 
         System.out.println("[Fim] - metodo2");
     }
