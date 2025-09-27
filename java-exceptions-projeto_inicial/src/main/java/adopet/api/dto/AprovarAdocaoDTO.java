78152e129344068d7274f3524d41c05e02538b6e
cruzmenezes <cruz.menezes@outlook.com>
2025-09-27 16:35:43 -0300

    atualizações

--- java-exceptions-projeto_inicial/src/main/java/adopet/api/dto/AprovarAdocaoDTO.java
+++ java-exceptions-projeto_inicial/src/main/java/adopet/api/dto/AprovarAdocaoDTO.java
@@ -2,5 +2,9 @@ package adopet.api.dto;
 
 import jakarta.validation.constraints.NotNull;
 
-public record AprovarAdocaoDTO(@NotNull Long idAdocao) {
+public record AprovarAdocaoDTO(@NotNull Long idAdocao, String justificativa) {
+
+    public String justificativa() {
+        return justificativa; // retorna o valor do campo
+    }
 }
